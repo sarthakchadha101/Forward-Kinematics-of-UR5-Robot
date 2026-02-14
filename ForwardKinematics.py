b781a24422a162a3f0a40348dc1fedb5efe2d6b8
sarthakchadha101 <sarthakchadha101@gmail.com>
2026-02-14 19:25:03 +0100

    Setting Joint Limits

--- ForwardKinematics.py
+++ ForwardKinematics.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 a, d = [0, -0.425, -0.39225, 0, 0, 0], [0.089159, 0, 0, 0.10915, 0.09465, 0.0823]
 alpha = [np.pi / 2, 0, 0, np.pi / 2, -np.pi / 2, 0]
+joint_limits = [
+    (-360, 360),  #(Base)
+    (-180, 180),  #(Shoulder)
+    (-180, 180),  #(Elbow)
+    (-360, 360),  #(Wrist 1)
+    (-360, 360),  #(Wrist 2)
+    (-360, 360)   #(Wrist 3)
+]
