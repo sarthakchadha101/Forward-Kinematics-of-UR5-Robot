c13b373e91a6ac212eb38dad905058036c5bf35a
sarthakchadha101 <sarthakchadha101@gmail.com>
2026-02-14 19:19:35 +0100

    initializing with DH parameters

--- ForwardKinematics.py
+++ ForwardKinematics.py
@@ -1,3 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+a, d = [0, -0.425, -0.39225, 0, 0, 0], [0.089159, 0, 0, 0.10915, 0.09465, 0.0823]
+alpha = [np.pi / 2, 0, 0, np.pi / 2, -np.pi / 2, 0]
